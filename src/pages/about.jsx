63afe968be9f526f76880defd8fe6af133916b12
Johnny Crisp <johnnycrisp2@gmail.com>
2023-01-08 14:20:48 +0100

    about page, archive page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,39 @@
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React from 'react'
+import Layout from '../components/Layout';
+import Seo from '../components/Seo';
 
-const about = () => {
+const about = ({data}) => {
+
+  const about = data.about
+  const image = about.aboutImage
+  console.log('about', about)
+
   return (
-    <div>about</div>
+    <Layout 
+    title={about.pageTitle}
+    >
+    <Seo title="About" />
+    <div dangerouslySetInnerHTML={{__html: about.aboutText}}>
+    </div>
+    <GatsbyImage alt='image of sam finney, cinematographer' className="about__image" image={getImage(image)}/>
+      
+    </Layout>
   )
 }
 
 export default about
+
+export const query = graphql`
+query AboutQuery {
+  about: datoCmsAbout {
+    originalId
+    pageTitle
+    aboutText
+    aboutImage {
+      gatsbyImageData(placeholder: BLURRED)
+    }
+  }
+}
+`
--- src/pages/archive.jsx
+++ src/pages/archive.jsx
@@ -1,9 +1,53 @@
+import { graphql } from 'gatsby'
 import React from 'react'
+import Layout from '../components/Layout'
+import Seo from '../components/Seo'
+import Thumbnail from '../components/Thumbnail'
 
-const archive = () => {
+const archive = ({data}) => {
+
+  const videos = data.videos.nodes;
+  const title = 'Archive'
+  console.log('vids', videos)
+
   return (
-    <div>archive</div>
+    <Layout 
+    // homepageText={data.datoCmsHomepage.homepageText}
+    title={title}
+    >
+    <Seo title="Archive" />
+    <div className="archive__grid">
+      {videos.map((video)=> {
+    return (
+        <Thumbnail key={video.originalId}
+        title={video.thumbnail[0].title}
+        subtitle={video.thumbnail[0].subtitle}
+        timestamp={video.thumbnail[0].videoTimestamp}
+        image={video.thumbnail[0].thumbnailImage}
+        slug={video.slug}
+         />
+    )
+  })}
+    </div>
+    </Layout>
   )
 }
 
 export default archive
+
+export const query = graphql`
+query ArchiveQuery {
+  videos: allDatoCmsVideo {
+   nodes {
+     originalId
+     thumbnail {
+        videoTimestamp
+        title
+        subtitle
+        thumbnailImage {
+          gatsbyImageData(placeholder: BLURRED)
+        }
+     }
+   }
+ }
+}`
