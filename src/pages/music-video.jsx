80f3a483094822ee5c73dca2faccdfdb004ad9a8
Johnny Crisp <johnnycrisp2@gmail.com>
2023-03-06 20:04:33 +0100

    music video page

--- src/pages/music-video.jsx
+++ src/pages/music-video.jsx
@@ -1,58 +1,55 @@
-// import React from 'react'
-// import Layout from '../components/Layout'
-// import { graphql } from "gatsby"
-// import Thumbnail from '../components/Thumbnail'
-// import Seo from '../components/Seo'
+import React from "react";
+import Layout from "../components/Layout";
+import { graphql } from "gatsby";
+import Thumbnail from "../components/Thumbnail";
+import Seo from "../components/Seo";
 
-// const Music = ({data}) => {
+const Music = ({ data }) => {
+  const thumbnails = data.datoCmsMusicVideo.videoThumbnails;
+  const title = data.datoCmsMusicVideo.pageTitle;
+  console.log("videos", thumbnails);
+  return (
+    <>
+      <Layout title={title}>
+        <Seo title="Music Video" />
+        <div className="archive__grid">
+          {thumbnails.map((thumbnail) => {
+            const vid = thumbnail.linkToVideo;
+            return (
+              <Thumbnail
+                key={vid.originalId}
+                title={vid.title}
+                subtitle={vid.subtitle}
+                timestamp={vid.videoTimestamp}
+                image={vid.thumbnailImage}
+                slug={thumbnail.linkToVideo.slug}
+              />
+            );
+          })}
+        </div>
+      </Layout>
+    </>
+  );
+};
 
-//   const thumbnails = data.datoCmsMusicVideo.videoThumbnails
-//   const title = data.datoCmsMusicVideo.pageTitle
-//   console.log('videos', thumbnails);
-//   return (
-//     <>
-//     <Layout
-//     title={title}
-//     >
-//     <Seo title="Music Video" />
-//     <div className="archive__grid">
-//       {thumbnails.map((thumbnail)=> {
-//     const vid = thumbnail.linkToVideo
-//     return (
-//         <Thumbnail key={vid.originalId}
-//         title={vid.title}
-//         subtitle={vid.subtitle}
-//         timestamp={vid.videoTimestamp}
-//         image={vid.thumbnailImage}
-//         slug={thumbnail.linkToVideo.slug}
-//         />
-//     )
-//   })}
-//     </div>
-//     </Layout>
-//     </>
+export default Music;
 
-//   )
-// }
-
-// export default Music
-
-// export const query = graphql`
-// query MusicQuery {
-//   datoCmsMusicVideo {
-//     pageTitle
-//     videoThumbnails {
-//       linkToVideo {
-//           slug
-//           originalId
-//           title
-//           subtitle
-//           videoTimestamp
-//           thumbnailImage {
-//             gatsbyImageData(placeholder: BLURRED)
-//           }
-//       }
-//     }
-//   }
-// }
-// `
+export const query = graphql`
+  query MusicQuery {
+    datoCmsMusicVideo {
+      pageTitle
+      videoThumbnails {
+        linkToVideo {
+          slug
+          originalId
+          title
+          subtitle
+          videoTimestamp
+          thumbnailImage {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+      }
+    }
+  }
+`;
